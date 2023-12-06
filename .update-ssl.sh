dd7f3cdddaff47a7519339d9848917e10add7c37
angelosorno <angel@moratoo.com>
2023-12-06 10:13:22 -0500

    Update .sh SSL Update

--- .update-ssl.sh
+++ .update-ssl.sh
@@ -6,14 +6,17 @@ echo "👇 Test de funcionalidad de CertBot"
 sudo certbot renew --dry-run
 
 # Revisar nuevos .conf y renovar SSLs existentes y los nuevos
+echo "👇 Renovando certificados..."
 sudo certbot renew --nginx -v
 
 # Reiniciar el servicio
+echo "👇 Reiniciando Nginx..."
 sudo nginx -t
 sudo service nginx restart
 
 # Revisar logs
+echo "👇 Estado del servicio Nginx..."
 systemctl status nginx.service
-echo "👇😀"
 # Mostrar mensaje de finalización
+echo "👇😀"
 echo "El script se ha completado correctamente ✅"
